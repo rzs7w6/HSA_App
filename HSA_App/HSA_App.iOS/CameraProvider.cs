22a7260c2a4c9c7feff7012e5e1775ba463596b8
rzs7w6 <rzs7w6@mail.missouri.edu>
2017-02-15 18:58:43 -0600

    Android camera working.

--- HSA_App/HSA_App.iOS/CameraProvider.cs
+++ HSA_App/HSA_App.iOS/CameraProvider.cs
@@ -9,18 +9,51 @@ using UIKit;
 [assembly: Dependency(typeof(CameraProvider))]
 namespace HSA_App.iOS
 {
-    class CameraProvider : ICameraProvider
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using UIKit;
+    using Foundation;
+
+    using HSA_App;
+
+    public class CameraProvider : ICameraProvider
     {
         public Task<CameraResult> TakePictureAsync()
         {
             var tcs = new TaskCompletionSource<CameraResult>();
 
-            // Use the camera helper to launch the picker     
-            Camera.TakePicture(
-                UIApplication.SharedApplication.KeyWindow.RootViewController,
-                (imagePickerResult) => {
-                // In the callback set the task result to the new picture using
-                // tcs.TrySetResult(..)
+            Camera.TakePicture(UIApplication.SharedApplication.KeyWindow.RootViewController, (imagePickerResult) => {
+
+                if (imagePickerResult == null)
+                {
+                    tcs.TrySetResult(null);
+                    return;
+                }
+
+                var photo = imagePickerResult.ValueForKey(new NSString("UIImagePickerControllerOriginalImage")) as UIImage;
+
+                // You can get photo meta data with using the following
+                // var meta = obj.ValueForKey(new NSString("UIImagePickerControllerMediaMetadata")) as NSDictionary;
+
+                var documentsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                string jpgFilename = Path.Combine(documentsDirectory, Guid.NewGuid() + ".jpg");
+                NSData imgData = photo.AsJPEG();
+                NSError err = null;
+
+                if (imgData.Save(jpgFilename, false, out err))
+                {
+                    CameraResult result = new CameraResult();
+                    result.Picture = ImageSource.FromStream(imgData.AsStream);
+                    result.FilePath = jpgFilename;
+
+                    tcs.TrySetResult(result);
+                }
+                else
+                {
+                    tcs.TrySetException(new Exception(err.LocalizedDescription));
+                }
             });
 
             return tcs.Task;
