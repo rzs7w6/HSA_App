cb88010a7c2b51373a360b6bd855303cf2758ea7
Bryan <Bryan@DESKTOP-HDIK3M2>
2017-03-14 16:12:58 -0500

    Removed RegistrationParser.cs, added get() to Rest to return user by username, added CoManager.xaml.cs which connects to the restful service, started updating the LoginPage to get a user.

---
+++ HSA_REST/HSA_REST/restservice.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HSA_REST
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "restservice" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select restservice.svc or restservice.svc.cs at the Solution Explorer and start debugging.
+    public class restservice : Irestservice
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
