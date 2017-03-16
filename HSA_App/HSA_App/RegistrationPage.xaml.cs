953ee17f36520c1aa6c454b3bb179c4f9a5bdde0
Kenrick Niedbalski <kjnzr3@mail.missouri.edu>
2017-03-15 22:08:54 -0500

    Can Register a User
    sucessful connection to restful service and to db

--- HSA_App/HSA_App/RegistrationPage.xaml.cs
+++ HSA_App/HSA_App/RegistrationPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 using Xamarin.Forms;
 
@@ -14,9 +15,10 @@ namespace HSA_App
 
 		public void handleReg(object sender, EventArgs e)
 		{
+            Debug.WriteLine("\n\n\nGot into the function\n\n\n");
 			int successFlag;
 			successFlag = 1;
-
+            /*
 			if (successFlag == 0)
 			{
 				username.Text = "USERNAME ALREADY TAKEN";
@@ -25,7 +27,24 @@ namespace HSA_App
 			else
 			{
 				App.Current.MainPage = new LoginPage();
-			}
-		}
+			}*/
+            
+            //regButton.Clicked += async (thissender, thise) =>
+            //{
+
+                User person = new User();
+                person.FirstName = fname.Text;
+                person.LastName = lname.Text;
+                person.UserName = username.Text;
+                person.AccountNumber = Convert.ToInt32(accountNum.Text);
+                //person.Birthday = Convert.ToDateTime(dobLabel.Text);
+                person.HashedPassword = password2.Text;
+                Debug.WriteLine("\n\n\n\ngot here\n\n\n\n");
+                var sv = new WebService();
+                var es = sv.RegisterUser(person);
+                App.Current.MainPage = new LandingPage();
+            //Debug.WriteLine("\n\nfound " + es.Length + " users\n\n");
+            //};
+        }
 	}
 }
