ea2e52987079267f3bda2d2e03270fe2a9326de7
Jackson Chandler <jackson.c.chandler@gmail.com>
2017-04-07 15:49:20 -0500

    Coupon Page doesnt crash now

--- HSA_App/HSA_App/CouponPage.xaml.cs
+++ HSA_App/HSA_App/CouponPage.xaml.cs
@@ -11,44 +11,42 @@ using Xamarin.Forms;
 
 namespace HSA_App
 {
-	public partial class CouponPage : ContentPage
-	{
-		List<Coupon> couponList = new List<Coupon>();
-		string[] TestData = { "CVS", "Free Shit", "Hyvee", "Not Free Shit" };
-
-		public CouponPage()
-		{
-			InitializeComponent();
-			GetCoupons();
-			list.ItemsSource = couponList;
-		}
-
-		private void GetCoupons()
-		{
-			/*  var rxcui = "198440";
-              var request = HttpWebRequest.Create(string.Format(@"http://rxnav.nlm.nih.gov/REST/RxTerms/rxcui/{0}/allinfo", rxcui));
-              request.ContentType = "application/json";
-              request.Method = "GET";
-              String[] items;
-              int counter = 0;
-              using (HttpWebResponse response = request.BeginGetResponse)
-              {
-                  using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+    public partial class CouponPage : ContentPage
+    {
+        List<Coupon> couponList = new List<Coupon>();
+        public string[] testData = { "CVS", "Free Shit", "Hyvee", "Not Free Shit" };
+        
+        public CouponPage()
+        {
+           InitializeComponent();
+        }
+        private void GetCoupons(object sender, EventArgs e)
+        {
+                /*  var rxcui = "198440";
+                  var request = HttpWebRequest.Create(string.Format(@"http://rxnav.nlm.nih.gov/REST/RxTerms/rxcui/{0}/allinfo", rxcui));
+                  request.ContentType = "application/json";
+                  request.Method = "GET";
+                  String[] items;
+                  int counter = 0;
+                  using (HttpWebResponse response = request.BeginGetResponse)
                   {
-                      String content = reader.ReadToEnd();
-                      while(counter < content.Length)
+                      using (StreamReader reader = new StreamReader(response.GetResponseStream()))
                       {
+                          String content = reader.ReadToEnd();
+                          while(counter < content.Length)
+                          {
+                          }
                       }
-                  }
-              }*/
-			for (int i = 0; i < TestData.Length / 2; i++)
-			{
-				Coupon coupon = new Coupon();
-				coupon.StoreName = TestData[i];
-				coupon.CouponDetails = TestData[i++];
-				couponList.Add(coupon);
-			}
+                  }*/
+                for (int i = 0; i < testData.Length / 2; i++)
+                {
+                    Coupon coupon = new Coupon();
+                    coupon.StoreName = testData[i];
+                    coupon.CouponDetails = testData[i++];
+                    couponList.Add(coupon);
+                }
+                
 
-		}
-	}
+            }
+    }
 }
