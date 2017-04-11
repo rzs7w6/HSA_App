5ba6f4f915e8bdf5194d30962982f7b1a1a39002
Emma Schiermeier <ersggb@mail.missouri.edu>
2017-04-10 23:03:22 -0500

    Added a comment so I could convince GitHub I made changes

--- HSA_App/HSA_App/BooleanToObjectConverter<T>.cs
+++ HSA_App/HSA_App/BooleanToObjectConverter<T>.cs
@@ -10,6 +10,8 @@ namespace HSA_App
 {
 	public class BooleanToObjectConverter<T> : IValueConverter
     {
+		//When creating this class the panel was correct but the class named itself "BooleanToObjectConverter_T_
+		//if anything goes wrong with referencing there's a good chance it's here
         public T FalseObject { set; get; }
 
 		public T TrueObject { set; get; }
